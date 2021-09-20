406605b526256399c46cf56dc477b0eeb0a1d3f8
theotrg <huntermonk2307@gmail.com>
2021-09-21 00:01:43 +0300

    almost finished with the js part, styling remains

--- app.js
+++ app.js
@@ -1,62 +1,82 @@
-// SELECTORS
-const iconElement = document.querySelector(".weather-icon");
-const tempElement = document.querySelector(".temperature-value p");
-const descElement = document.querySelector(".temperature-description p");
-const locationElement = document.querySelector(".location p");
-const notificationElement = document.querySelector("notification");
 
+const city_country = document.querySelector('.city-country');
+const temp = document.querySelector('.temperature p');
+const description = document.querySelector('.description p');
+const feelsLike = document.querySelector('.feels-like');
+const humidity = document.querySelector('.humidity');
+const wind = document.querySelector('.wind');
+const notification = document.querySelector('.notification');
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('temperature p');
 
 const weather = {};
 
-weather.temperature = {
-    unit: "celsius"
-}
+// weather.temperature = {
+//     unit: 'celsius'
+// }
 
-const KELVIN = 273;
-const key = "9bb8e44608705302e495ca23fbec8cbd";
+KELVIN = 273;
+ 
+const key = '9bb8e44608705302e495ca23fbec8cbd';
+    
 
 
-if("geolocation" in navigator){
-    navigator.geolocationCurrentPosition(setPosition, showError);
+// CHECK IF BROWSER SUPPORTS GEOLOCATION SERVICES
+if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
 }else{
-    notificationElement.style.display = 'block';
-    notificationElement.innerHTML = "<p>Browser doesn't support Geolocation</p>";
+    notification.style.display = 'block';
+    notification.innerHTML = "<p>Browser doesn't support GeoLocation</p>";
 }
 
+// GET USER'S POSITION
 function setPosition(position){
     let latitude = position.coords.latitude;
-    let longitude = position.coord.longitude;
-    
+    let longitude = position.coords.longitude;
+
     getWeather(latitude, longitude);
 }
 
 function showError(error){
-    notificationElement.style.display = 'block';
-    notificationElement.innerHTML = `<p>${error.message}</p>`;
+    notification.style.display = 'block';
+    notification.innerHTML = `<p>${error.message}</p>`
 }
 
-function getWeather(latitude, longitude){
-    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
 
+function getWeather(latitude, longitude){
+    let api =`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`; 
+    console.log(api);
     fetch(api)
-        .then(function (response){
+        .then(function(response){
             let data = response.json();
             return data;
         })
-        .then(function(data){
-            weather.temperature.value = Math.floor(data.main.temp - KELVIN),
-            weather.description = data.weather[0].icon;
-            weather.iconId = data.weather[0].icon;
-            weather.city = data.name;
-            weather.country = data.sys.country;
 
+        .then(function(data){
+            weather.city = data.name;
+            weather.country = data.sys.country
+            weather.iconId = data.weather[0].icon;
+            weather.temperature = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].description;
+            weather.feelsLike = Math.floor(data.main.feels_like - KELVIN);
+            weather.humidity = data.main.humidity;
+            weather.wind = data.wind.speed;
+            
+            
         })
 
         .then(function(){
-            displayWeather();
+            displayWeather()
         })
 }
 
 function displayWeather(){
-    iconElement.innerHTML = 
+    city_country.textContent = `${weather.city}, ${weather.country}`;
+    // weatherIcon.innerHTML = `<img src="icons/${iconId}.png">`;
+    // temperature.innerHTML = `${weather.temperature}°<span>C</span>`;
+    description.textContent = `${weather.description}`;
+    feelsLike.innerHTML = `<p>Feels like: ${weather.feelsLike}<span>°</span>C</p>`;
+    wind.innerHTML = `<p>Wind: ${weather.wind}<span>m/s</span></p>`;
+    humidity.innerHTML = `<p>Humidity: ${weather.humidity}<span>%</span><p>`
+
 }
